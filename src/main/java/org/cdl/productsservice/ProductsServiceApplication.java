0fc77c4a1ec031f6e21583a9eaeef9b59c0b3700
Joseph Barker <jbarker@csumb.edu>
2021-08-18 22:24:06 -0700

    added health check

--- src/main/java/org/cdl/productsservice/ProductsServiceApplication.java
+++ src/main/java/org/cdl/productsservice/ProductsServiceApplication.java
@@ -2,11 +2,19 @@ package org.cdl.productsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProductsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsServiceApplication.class, args);
+	}
+
+	@GetMapping
+	public String healthCheck() {
+		return "HEALTH CHECK OK!";
 	}
 }
